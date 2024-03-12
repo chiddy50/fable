985ae88336268075021365b62fd101ad6bed0899
Chidi Michael <43938389+chiddy50@users.noreply.github.com>
2024-03-12 16:39:18 +0100

    First commit

--- components/challenge/new-challenge2.tsx
+++ components/challenge/new-challenge2.tsx
@@ -64,6 +64,7 @@ const  NewChallenge2 = () => {
 
     const textAreaBlured = (question: Question) => {
         // setFocusedQuestion(null)
+        fixCompletionRate()
     }
 
     function updateAnswer(e: React.ChangeEvent<HTMLTextAreaElement>, question: Question){
@@ -80,18 +81,6 @@ const  NewChallenge2 = () => {
             });
             setAllQuestions(allQuestions)
 
-
-
-
-
-            const totalQuestions = 8;            
-            let answeredQuestions = 0;
-            questions.forEach(category => {
-                if (category.answer !== "") answeredQuestions++;
-            });
-
-            const completion_rate = (answeredQuestions / totalQuestions) * 100;
-            setCompletionRate(Math.floor(completion_rate))
         }
     }
 
@@ -101,6 +90,17 @@ const  NewChallenge2 = () => {
             return false;
         }
         // router.push('/user/summary')
+    }
+
+    const fixCompletionRate = () => {
+        const totalQuestions = 8;            
+        let answeredQuestions = 0;
+        questions.forEach(category => {
+            if (category.answer !== "") answeredQuestions++;
+        });
+
+        const completion_rate = (answeredQuestions / totalQuestions) * 100;
+        setCompletionRate(Math.floor(completion_rate))
     }
 
     const validateForm = () => {
@@ -135,16 +135,16 @@ const  NewChallenge2 = () => {
                     focusedQuestion && 
                     <div className=" bg-white p-5 rounded-lg shadow-sm drop-shadow-sm col-span-3">
                         <div className=" flex justify-between font-mono text-sm font-semibold  mb-4 tracking-widest text-gray-500 ">
-                            <p>
+                            <p className='xs:text-xs sm:text-xs md:text-sm'>
                                 <span className='text-xl font-bold'>0{focusedQuestion?.index}:</span> {focusedQuestion?.title}
                             </p>
-                            <div className=" flex space-x-8">                        
+                            {/* <div className=" flex space-x-8">                        
                                 <div>
                                     <button>
                                         <svg className=" w-4 h-4 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
                                     </button>
                                 </div>
-                            </div>
+                            </div> */}
                         </div>
                         {
                             focusedQuestion?.questions.map((question, index) => (
@@ -155,7 +155,7 @@ const  NewChallenge2 = () => {
                         <p className=" w-full text-xs mt-7 font-mono text-gray-800 tracking-widest leading-4">
                             { focusedQuestion?.answer }
                         </p>
-                        <div className=" flex w-full justify-between mt-6">
+                        {/* <div className=" flex w-full justify-between mt-6">
                             <div>
                                 <button>
                                     <svg className=" w-4 h-4 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M364.2 83.8C339.8 59.39 300.2 59.39 275.8 83.8L91.8 267.8C49.71 309.9 49.71 378.1 91.8 420.2C133.9 462.3 202.1 462.3 244.2 420.2L396.2 268.2C407.1 257.3 424.9 257.3 435.8 268.2C446.7 279.1 446.7 296.9 435.8 307.8L283.8 459.8C219.8 523.8 116.2 523.8 52.2 459.8C-11.75 395.8-11.75 292.2 52.2 228.2L236.2 44.2C282.5-2.08 357.5-2.08 403.8 44.2C450.1 90.48 450.1 165.5 403.8 211.8L227.8 387.8C199.2 416.4 152.8 416.4 124.2 387.8C95.59 359.2 95.59 312.8 124.2 284.2L268.2 140.2C279.1 129.3 296.9 129.3 307.8 140.2C318.7 151.1 318.7 168.9 307.8 179.8L163.8 323.8C157.1 330.5 157.1 341.5 163.8 348.2C170.5 354.9 181.5 354.9 188.2 348.2L364.2 172.2C388.6 147.8 388.6 108.2 364.2 83.8V83.8z"/></svg>
@@ -166,13 +166,13 @@ const  NewChallenge2 = () => {
                                     <svg className=" w-4 h-4 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg>
                                 </button>
                             </div>
-                        </div>
+                        </div> */}
                         
                     </div>
                 }
                 
                 
-                <div className="space-x-5 inset-x-0 fixed bottom-0 w-full justify-start px-7 py-4">
+                <div className="space-x-5 inset-x-0 fixed bottom-0 w-full justify-start px-0 py-4">
                         
                     <div className='w-full flex justify-center items-center gap-5'>
                         { completionRate < 100 &&
